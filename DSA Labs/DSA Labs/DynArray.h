da63ded28e23ffb21d178a38346f15b305007659
nikoflame <piercenik@gmail.com>
2024-05-06 20:37:43 -0400

    lab 1 complete

--- DSA Labs/DSA Labs/DynArray.h
+++ DSA Labs/DSA Labs/DynArray.h
@@ -3,7 +3,7 @@ File:			DynArray.h
 Author(s):
 	Base:		Justin Tackett
 				jtackett@fullsail.com
-	Student:
+	Student:	Nicholas Pierce
 
 Purpose:		A dynamically-allocated resizeable array (similar to std::vector)
 Notes:			Property of Full Sail University 2402
@@ -31,24 +31,24 @@ NOTE: If the unit test is not on, that code will not be compiled!
 */
 
 // Main toggle
-#define LAB_1	0
+#define LAB_1	1
 
 // Individual unit test toggles
-#define LAB1_DEFAULT_CONSTRUCTOR_NO_ARGS			0
-#define LAB1_DEFAULT_CONSTRUCTOR_WITH_ARGS			0
-#define LAB1_BRACKET_OPERATOR						0
-#define LAB1_SIZE_ACCESSOR							0
-#define LAB1_CAPACITY_ACCESSOR						0
-#define LAB1_RESERVE_EMPTY							0
-#define LAB1_RESERVE_DOUBLE_CAPACITY				0
-#define LAB1_RESERVE_LARGER_CAPACITY				0
-#define LAB1_RESERVE_SMALLER_CAPACITY				0
-#define LAB1_APPEND_NO_RESIZE						0
-#define LAB1_APPEND_RESIZE							0
-#define LAB1_CLEAR									0
-#define LAB1_DESTRUCTOR								0
-#define LAB1_ASSIGNMENT_OPERATOR					0
-#define LAB1_COPY_CONSTRUCTOR						0
+#define LAB1_DEFAULT_CONSTRUCTOR_NO_ARGS			1
+#define LAB1_DEFAULT_CONSTRUCTOR_WITH_ARGS			1
+#define LAB1_BRACKET_OPERATOR						1
+#define LAB1_SIZE_ACCESSOR							1
+#define LAB1_CAPACITY_ACCESSOR						1
+#define LAB1_RESERVE_EMPTY							1
+#define LAB1_RESERVE_DOUBLE_CAPACITY				1
+#define LAB1_RESERVE_LARGER_CAPACITY				1
+#define LAB1_RESERVE_SMALLER_CAPACITY				1
+#define LAB1_APPEND_NO_RESIZE						1
+#define LAB1_APPEND_RESIZE							1
+#define LAB1_CLEAR									1
+#define LAB1_DESTRUCTOR								1
+#define LAB1_ASSIGNMENT_OPERATOR					1
+#define LAB1_COPY_CONSTRUCTOR						1
 
 // Our implementation of a vector (simplified)
 template<typename Type>
@@ -67,51 +67,88 @@ public:
 
 	DynArray(size_t _startingCap = 0) {
 		// TODO: Implement this method
-		
+		mSize = 0;
+
+		if (_startingCap == 0) {
+			mArray = nullptr;
+			mCapacity = 0;
+		}
+		else {
+			mArray = new Type[_startingCap];
+			mCapacity = _startingCap;
+		}
 	};
 
 	~DynArray() {
 		// TODO: Implement this method
-
+		Clear();
 	}
 
 	DynArray(const DynArray& _copy) {
 		// TODO: Implement this method
-
+		mArray = nullptr;
+		mSize = 0;
+		mCapacity = 0;
+		*this = _copy;
 	}
 
 	DynArray& operator=(const DynArray& _assign) {
-		// TODO: Implement this method
+		// TODO: Implement this method*
+		if (this == &_assign) return *this;
 
+		Clear();
+		mArray = new Type[_assign.mCapacity];
+		for (int i = 0; i < _assign.mSize; i++)
+			mArray[i] = _assign.mArray[i];
+		mSize = _assign.mSize;
+		mCapacity = _assign.mCapacity;
+		return *this;
 	}
 
 	void Clear() {
 		// TODO: Implement this method
-
+		delete[] mArray;
+		mArray = nullptr;
+		mSize = 0;
+		mCapacity = 0;
 	}
 
 	Type& operator[](size_t _index) {
 		// TODO: Implement this method
-	
+		return mArray[_index];
 	}
 
 	size_t Size() const {
 		// TODO: Implement this method
-
+		return mSize;
 	}
 
 	size_t Capacity() const {
 		// TODO: Implement this method
-	
+		return mCapacity;
 	}
 
 	void Append(const Type& _data) {
 		// TODO: Implement this method
-	
+		if (mCapacity == mSize) Reserve();
+		mArray[mSize] = _data;
+		mSize++;
 	}
 
 	void Reserve(size_t _newCapacity = 0) {
 		// TODO: Implement this method
+		if (_newCapacity == 0) {
+			if (!mCapacity) mCapacity = 1;
+			else mCapacity *= 2;
+		}
+		else if (_newCapacity > mCapacity) mCapacity = _newCapacity;
+		else return;
 
+		Type* newArray = new Type[mCapacity];
+		for (int i = 0; i < mSize; i++) {
+			newArray[i] = mArray[i];
+		}
+		delete[] mArray;
+		mArray = newArray;
 	}
 };
